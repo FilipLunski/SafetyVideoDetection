98c57018f3c604f8a841dcbace3ecba82cea8332
Filip Lunski <f.lunski@gmail.com>
2025-05-23 14:18:53 +0200

    labels

--- dataset_creators/yolo_test.py
+++ dataset_creators/yolo_test.py
@@ -1,22 +1,25 @@
 from ultralytics import YOLO
 import os
 import glob
+from concurrent.futures import ThreadPoolExecutor
 
 
 pose_model = YOLO("./models_pose/yolo11x-pose")
 
 
-def main(video_folder, out_video_folder, input_format="mp4"):
+def main(video_folder, input_format="mp4"):
 
-    if (not os.path.exists(out_video_folder)):
-        os.makedirs(out_video_folder)
+    video_files = glob.glob(os.path.join(video_folder, f"*.{input_format}"))
 
-    for file in glob.glob(video_folder + "\\*." + input_format):
-        print('\n', file, end='\t')
-        pose_model.track(file, tracker="bytetrack.yaml",
-                         show=False, verbose=False, save=True)
+    for file in video_files:
+        process_video(file)
 
 
+def process_video(file):
 
-main('samples\\video\\MCFD',
-     'samples\\video\\MCFD\\output', "mp4")
+    print('\n', file, "\t------------------------------------------------------------------------------------------------------------------------------------")
+    pose_model.track(file, tracker="bytetrack.yaml",
+                     show=False, verbose=False, save=True)
+
+
+main('samples\\video\\MCFD', "mp4")
